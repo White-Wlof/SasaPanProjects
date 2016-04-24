21906b9b8a8c4fde8c8501af377b415b30e1fc66
sho <cisy13112@g.nihon-u.ac.jp>
2016-04-24 11:11:40 +0900

    make river

---
+++ SasaProjects/Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+	
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //this.transform.position += new Vector3(-0.1f, 0, 0);
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(-10, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //this.transform.position += new Vector3(0.1f, 0, 0);
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(10, 0, 0));
+        }
+    }
+}
